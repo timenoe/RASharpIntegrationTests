1b6f2e382a1562c769475bde1908f55095adafb8
Tim Long <timlong105@gmail.com>
2025-01-04 09:44:15 -0500

    Initial commit

---
+++ Network/NetworkRequestTests.cs
@@ -0,0 +1,59 @@
+﻿using RetroAchievementsMod.Network;
+
+namespace RetroAchievementsModTests.Network
+{
+    [TestClass]
+    public sealed class RequestManagerTests
+    {
+        [TestMethod]
+        public void BuildLoginRequestTest()
+        {
+            RequestManager.BuildLoginRequest("retroachievements.org", "TimmoneSimmons", "supersecretpass", out UriBuilder request);
+            Assert.AreEqual("https://retroachievements.org/dorequest.php?r=login2&u=TimmoneSimmons&p=supersecretpass", request.Uri.ToString());
+        }
+
+        [TestMethod]
+        public void BuildStartSessionRequestTest()
+        {
+            RequestManager.BuildStartSessionRequest("retroachievements.org", "TimmoneSimmons", "0123456789abcdef", 32123, out UriBuilder request);
+            Assert.AreEqual("https://retroachievements.org/dorequest.php?r=startsession&u=TimmoneSimmons&t=0123456789abcdef&g=32123", request.Uri.ToString());
+        }
+
+        [TestMethod]
+        public async Task BuildPingRequestTest()
+        {
+            RequestManager.BuildPingRequest("retroachievements.org", "TimmoneSimmons", "0123456789abcdef", 32123, "Digging a hellevator 👌", out UriBuilder request, out MultipartFormDataContent multipart);
+            Assert.AreEqual("https://retroachievements.org/dorequest.php?r=ping&u=TimmoneSimmons&t=0123456789abcdef&g=32123", request.Uri.ToString());
+
+            Assert.IsNotNull(multipart.First().Headers.ContentDisposition);
+            Assert.AreEqual("m", multipart.First().Headers.ContentDisposition?.Name);
+            Assert.AreEqual("Digging a hellevator 👌", await multipart.First().ReadAsStringAsync());
+        }
+
+        [TestMethod]
+        public void BuildAwardAchievementTest()
+        {
+            RequestManager.BuildAwardAchievementRequest("retroachievements.org", "TimmoneSimmons", "0123456789abcdef", true, 32123, out UriBuilder request);
+            Assert.AreEqual("https://retroachievements.org/dorequest.php?r=awardachievement&u=TimmoneSimmons&t=0123456789abcdef&h=1&a=32123&v=7a3f30386627952180d5afbae3beee6f", request.Uri.ToString());
+        }
+
+        [TestMethod]
+        public async Task BuildAwardAchievementsRequestTest()
+        {
+            RequestManager.BuildAwardAchievementsRequest("retroachievements.org", "TimmoneSimmons", "0123456789abcdef", true, [483244, 483245, 483246], out UriBuilder request, out MultipartFormDataContent multipart);
+            Assert.AreEqual("https://retroachievements.org/dorequest.php?r=awardachievements&u=TimmoneSimmons&t=0123456789abcdef", request.Uri.ToString());
+
+            Assert.IsNotNull(multipart.First().Headers.ContentDisposition);
+            Assert.AreEqual("h", multipart.First().Headers.ContentDisposition?.Name);
+            Assert.AreEqual("1", await multipart.First().ReadAsStringAsync());
+
+            Assert.IsNotNull(multipart.ElementAt(1).Headers.ContentDisposition);
+            Assert.AreEqual("a", multipart.ElementAt(1).Headers.ContentDisposition?.Name);
+            Assert.AreEqual("483244,483245,483246", await multipart.ElementAt(1).ReadAsStringAsync());
+
+            Assert.IsNotNull(multipart.ElementAt(2).Headers.ContentDisposition);
+            Assert.AreEqual("v", multipart.ElementAt(2).Headers.ContentDisposition?.Name);
+            Assert.AreEqual("9e9cf35e8d0c9fc0cc26061b556ce226", await multipart.ElementAt(2).ReadAsStringAsync());
+        }
+    }
+}
---
+++ Network/NetworkResponseTests.cs
@@ -0,0 +1,180 @@
+﻿using RetroAchievementsMod.Network;
+using RetroAchievementsMod.Utils;
+
+namespace RetroAchievementsModTests.Network
+{
+    [TestClass]
+    public class PingResponseTests
+    {
+        [TestMethod]
+        public void SuccessTest()
+        {
+            string json = "{\"Success\":true}";
+            LoginResponse response = new(JsonUtil.DeserializeJson(json));
+            Assert.IsTrue(response.success);
+            Assert.AreEqual(ResponseType.SUCCESS, response.type);
+        }
+
+
+        [TestMethod]
+        public void EmptyTest()
+        {
+            // Empty string
+            string json = "";
+            PingResponse response = new(JsonUtil.DeserializeJson(json));
+            Assert.IsFalse(response.success);
+            Assert.AreEqual(ResponseType.EMPTY, response.type);
+
+            // Empty Dictionary
+            json = "{}";
+            response = new(JsonUtil.DeserializeJson(json));
+            Assert.IsFalse(response.success);
+            Assert.AreEqual(ResponseType.EMPTY, response.type);
+        }
+    }
+
+    [TestClass]
+    public class LoginResponseTests
+    {
+        [TestMethod]
+        public void SuccessTest()
+        {
+            string json = "{\"Success\":true,\"User\":\"TimmoneSimmons\",\"Token\":\"0123456789abcdef\",\"Score\":69420,\"SoftcoreScore\":1337,\"Messages\":99,\"Permissions\":3,\"AccountType\":\"Developer\"}";
+            LoginResponse response = new(JsonUtil.DeserializeJson(json));
+            Assert.IsTrue(response.success);
+            Assert.AreEqual("TimmoneSimmons", response.user);
+            Assert.AreEqual("0123456789abcdef", response.token);
+            Assert.AreEqual(69420, response.score);
+            Assert.AreEqual(1337, response.softScore);
+            Assert.AreEqual(99, response.messages);
+            Assert.AreEqual(3, response.perms);
+            Assert.AreEqual("Developer", response.accountType);
+            Assert.AreEqual(ResponseType.SUCCESS, response.type);
+        }
+
+        [TestMethod]
+        public void ErrorTest()
+        {
+            string json = "{\"Success\":false,\"Error\":\"Invalid user\\/token combination.\",\"Code\":\"invalid_credentials\",\"Status\":401}";
+            PingResponse response = new(JsonUtil.DeserializeJson(json));
+            Assert.IsFalse(response.success);
+            Assert.AreEqual("Invalid user/token combination.", response.error);
+            Assert.AreEqual("invalid_credentials", response.code);
+            Assert.AreEqual(401, response.status);
+            Assert.AreEqual(ResponseType.ERROR, response.type);
+        }
+    }
+
+    [TestClass]
+    public class StartSessionResponseTests
+    {
+        [TestMethod]
+        public void SuccessTest()
+        {
+            string json = "{\"Success\":true,\"HardcoreUnlocks\":[{\"ID\":483244,\"When\":1735947878},{\"ID\":483245,\"When\":1735947888}],\"ServerNow\":1735947898}";
+            StartSessionResponse response = new(JsonUtil.DeserializeJson(json));
+            Assert.IsTrue(response.success);
+            Assert.AreEqual(2, response.unlocks.Count);
+            Assert.AreEqual(483244, response.unlocks[0]);
+            Assert.AreEqual(483245, response.unlocks[1]);
+            Assert.AreEqual(1735947898, response.serverTime);
+            Assert.AreEqual(ResponseType.SUCCESS, response.type);
+        }
+
+        [TestMethod]
+        public void ErrorTest()
+        {
+            string json = "{\"Success\":false,\"Error\":\"Unknown game\"}";
+            StartSessionResponse response = new(JsonUtil.DeserializeJson(json));
+            Assert.IsFalse(response.success);
+            Assert.AreEqual("Unknown game", response.error);
+            Assert.AreEqual(ResponseType.ERROR, response.type);
+        }
+    }
+
+    [TestClass]
+    public class AwardAchievementResponseTests
+    {
+        [TestMethod]
+        public void SuccessTest()
+        {
+            string json = "{\"Success\":true,\"AchievementsRemaining\":2,\"Score\":69420,\"SoftcoreScore\":1337,\"AchievementID\":483244}";
+            AwardAchievementResponse response = new(JsonUtil.DeserializeJson(json));
+            Assert.IsTrue(response.success);
+            Assert.AreEqual(2, response.achsRemaining);
+            Assert.AreEqual(69420, response.score);
+            Assert.AreEqual(1337, response.softScore);
+            Assert.AreEqual(483244, response.achId);
+            Assert.AreEqual(ResponseType.SUCCESS, response.type);
+        }
+
+        [TestMethod]
+        public void ErrorTestAlreadyUnlocked()
+        {
+            string json = "{\"Success\":false,\"AchievementsRemaining\":0,\"Error\":\"User already has this achievement unlocked.\",\"Score\":69420,\"SoftcoreScore\":1337,\"AchievementID\":213632}";
+            AwardAchievementResponse response = new(JsonUtil.DeserializeJson(json));
+            Assert.IsFalse(response.success);
+            Assert.AreEqual("User already has this achievement unlocked.", response.error);
+            Assert.AreEqual(69420, response.score);
+            Assert.AreEqual(1337, response.softScore);
+            Assert.AreEqual(213632, response.achId);
+            Assert.AreEqual(ResponseType.ERROR, response.type);
+        }
+
+        [TestMethod]
+        public void ErrorTestInvalid()
+        {
+            string json = "{\"Success\":false,\"Error\":\"Data not found for achievement 0\",\"Score\":69420,\"SoftcoreScore\":1337,\"AchievementID\":0}";
+            AwardAchievementResponse response = new(JsonUtil.DeserializeJson(json));
+            Assert.IsFalse(response.success);
+            Assert.AreEqual("Data not found for achievement 0", response.error);
+            Assert.AreEqual(69420, response.score);
+            Assert.AreEqual(1337, response.softScore);
+            Assert.AreEqual(0, response.achId);
+            Assert.AreEqual(ResponseType.ERROR, response.type);
+        }
+
+        [TestMethod]
+        public void ErrorTestUnofficial()
+        {
+            string json = "{\"Success\":false,\"Error\":\"Unofficial achievements cannot be unlocked\",\"Score\":69420,\"SoftcoreScore\":1337,\"AchievementID\":483244}";
+            AwardAchievementResponse response = new(JsonUtil.DeserializeJson(json));
+            Assert.IsFalse(response.success);
+            Assert.AreEqual("Unofficial achievements cannot be unlocked", response.error);
+            Assert.AreEqual(69420, response.score);
+            Assert.AreEqual(1337, response.softScore);
+            Assert.AreEqual(483244, response.achId);
+            Assert.AreEqual(ResponseType.ERROR, response.type);
+        }
+    }
+
+    [TestClass]
+    public class AwardAchievementsResponseTests
+    {
+        [TestMethod]
+        public void SuccessTest()
+        {
+            string json = "{\"Success\":true,\"Score\":69420,\"SoftcoreScore\":1337,\"ExistingIDs\":[483244],\"SuccessfulIDs\":[483245,483246]}";
+            AwardAchievementsResponse response = new(JsonUtil.DeserializeJson(json));
+            Assert.IsTrue(response.success);
+            Assert.AreEqual(69420, response.score);
+            Assert.AreEqual(1337, response.softScore);
+            Assert.AreEqual(1, response.existingIds.Count);
+            Assert.AreEqual(483244, response.existingIds[0]);
+            Assert.AreEqual(2, response.successfulIds.Count);
+            Assert.AreEqual(483245, response.successfulIds[0]);
+            Assert.AreEqual(483246, response.successfulIds[1]);
+            Assert.AreEqual(ResponseType.SUCCESS, response.type);
+        }
+
+        [TestMethod]
+        public void ErrorTestNoDelegation()
+        {
+            string json = "{\"Success\":false,\"Error\":\"You must specify a target user.\",\"Status\":400}";
+            AwardAchievementsResponse response = new(JsonUtil.DeserializeJson(json));
+            Assert.IsFalse(response.success);
+            Assert.AreEqual("You must specify a target user.", response.error);
+            Assert.AreEqual(400, response.status);
+        }
+    }
+}
